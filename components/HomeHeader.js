50f17150c6fd75b45431a0b3ddbad8e36f1da5d0
Ville Räsänen <ville.rasanen@hotmail.com>
2023-02-21 11:30:34 +0200

    simple app ready

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,10 +1,96 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, TextInput } from "react-native";
 
-const HomeHeader = () => {
+import { COLORS, FONTS, SIZES, assets } from "../constants";
+
+const HomeHeader = ({ onSearch }) => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View
+      style={{
+        backgroundColor: "#F2F2F2",
+        padding: SIZES.font,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={assets.logo}
+          resizeMode="contain"
+          style={{ width: 45, height: 45 }}
+        />
+
+        <View style={{ width: 45, height: 45 }}>
+          <Image
+            source={assets.person01}
+            resizeMode="contain"
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          />
+          <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{
+              position: "absolute",
+              width: 20,
+              height: 20,
+              bottom: 0,
+              right: 0,
+            }}
+          />
+        </View>
+      </View>
+      <View style={{ marginVertical: SIZES.font }}>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small,
+            color: COLORS.primary,
+          }}
+        >
+          Hello, Ville 👋
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.bold,
+            fontSize: SIZES.large,
+            color: COLORS.primary,
+            marginTop: SIZES.base / 2,
+          }}
+        >
+          Let's find a dogepiece
+        </Text>
+      </View>
+      <View style={{ marginTop: 5}}>
+        <View
+          style={{
+            width: "100%",
+            borderRadius: SIZES.font,
+            backgroundColor: COLORS.gray,
+            paddingHorizontal: SIZES.font,
+            paddingVertical: SIZES.small - 2,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={assets.search}
+            resizeMode="contain"
+            style={{ width: 20, height: 20, marginRight: SIZES.base }}
+          />
+          <TextInput
+            placeholder="Search DogFTs"
+            placeholderTextColor={COLORS.white}
+            style={{ flex: 1 }}
+            onChangeText={onSearch}
+          />
+        </View>
+      </View>
     </View>
   );
 };
--- screens/Home.js
+++ screens/Home.js
@@ -5,18 +5,38 @@ import { COLORS, NFTData } from "../constants";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (!value.length) return setNftData(NFTData);
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length) {
+      setNftData(filteredData);
+    } else {
+      setNftData(NFTData);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <FocusedStatusBar background={COLORS.primary} />
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        transLucent={true}
+      />
 
       <View style={{ flex: 1 }}>
-        <View style={{ zIndex: 0}}>
+        <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
+            data={nftData}
             renderItem={({ item }) => <NFTCard data={item} />}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch}/>}
           />
         </View>
 
@@ -30,7 +50,7 @@ const Home = () => {
             zIndex: -1,
           }}
         >
-          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ height: 300, backgroundColor: "#F2F2F2" }} />
           <View style={{ flex: 1, backgroundColor: COLORS.white }} />
         </View>
       </View>
